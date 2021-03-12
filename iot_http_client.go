a31a9566f9764422381b94abfdcc4d1a5632669f
ctlove0523 <478309639@qq.com>
2021-03-12 14:34:51 +0800

    use resty as default http client

--- iot_http_client.go
+++ iot_http_client.go
@@ -1,15 +1,11 @@
 package iot
 
 import (
-	"bytes"
+	"github.com/go-resty/resty/v2"
 	"github.com/golang/glog"
-	"io"
 	"io/ioutil"
-	"mime/multipart"
-	"net/http"
 	"net/url"
 	"os"
-	"time"
 )
 
 // 仅用于设备上传文件
@@ -19,7 +15,7 @@ type HttpClient interface {
 }
 
 type httpClient struct {
-	client *http.Client
+	client *resty.Client
 }
 
 func (client *httpClient) DownloadFile(fileName, downloadUrl string) bool {
@@ -30,37 +26,27 @@ func (client *httpClient) DownloadFile(fileName, downloadUrl string) bool {
 		glog.Errorf("create file in os failed ,file name %s", fileName)
 		return false
 	}
-	defer out.Close()
-
-	bodyBuffer := &bytes.Buffer{}
-	req, err := http.NewRequest("GET", downloadUrl, bodyBuffer)
-	if err != nil {
-		glog.Errorf("create request filed %v", err)
-		return false
-	}
-
-	req.Header.Add("Content-Type", "text/plain")
 
 	originalUri, err := url.ParseRequestURI(downloadUrl)
 	if err != nil {
 		glog.Errorf("parse request uri failed %v", err)
 		return false
 	}
-	req.Header.Add("Host", originalUri.Host)
 
-	res, err := client.client.Do(req)
+	resp, err := client.client.R().
+		SetHeader("Content-Type", "text/plain").
+		SetHeader("Host", originalUri.Host).
+		Get(downloadUrl)
+
 	if err != nil {
-		glog.Errorf("down load file error")
+		glog.Errorf("download file request failed %v", err)
 		return false
 	}
-	buf := make([]byte, 1024)
-	for {
-		size, _ := res.Body.Read(buf)
-		if size == 0 {
-			break
-		} else {
-			_, _ = out.Write(buf[:size])
-		}
+
+	_, err = out.Write(resp.Body())
+	if err != nil {
+		glog.Errorf("write file failed")
+		return false
 	}
 
 	return true
@@ -68,62 +54,39 @@ func (client *httpClient) DownloadFile(fileName, downloadUrl string) bool {
 
 func (client *httpClient) UploadFile(filename, uri string) bool {
 	filename = SmartFileName(filename)
-	bodyBuffer := &bytes.Buffer{}
-	bodyWriter := multipart.NewWriter(bodyBuffer)
+	fileBytes, err := ioutil.ReadFile(filename)
 
-	fileWriter, err := bodyWriter.CreateFormFile("files", filename)
 	if err != nil {
-		glog.Errorf("create form file failed %v", err)
+		glog.Errorf("read file failed %v", err)
 		return false
 	}
-
-	file, err := os.Open(filename)
-	if err != nil {
-		glog.Errorf("open file failed %v", err)
-		return false
-	}
-
-	defer file.Close()
-
-	_, err = io.Copy(fileWriter, file)
-	if err != nil {
-		glog.Errorf("copy file to writer failed %v", err)
-	}
-
-	//contentType := bodyWriter.FormDataContentType()
-	defer bodyWriter.Close()
-
-	req, err := http.NewRequest("PUT", uri, bodyBuffer)
-	if err != nil {
-		glog.Errorf("create request filed %v", err)
-	}
-
-	req.Header.Add("Content-Type", "text/plain")
 
 	originalUri, err := url.ParseRequestURI(uri)
 	if err != nil {
 		glog.Errorf("parse request uri failed %v", err)
+		return false
 	}
-	req.Header.Add("Host", originalUri.Host)
-	resp, _ := client.client.Do(req)
 
-	defer resp.Body.Close()
+	resp, err := client.client.R().
+		SetHeader("Content-Type", "text/plain").
+		SetHeader("Host", originalUri.Host).
+		SetBody(fileBytes).
+		Put(uri)
 
-	_, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		glog.Errorf("upload request failed %v", err)
+	}
 
-	return nil == err
+	return resp.StatusCode() == 200
 }
 
 func CreateHttpClient() HttpClient {
-	tr := &http.Transport{
-		MaxIdleConns:       10,
-		IdleConnTimeout:    30 * time.Second,
-		DisableCompression: true,
-	}
-	innerClient := &http.Client{Transport: tr}
+	client := resty.New()
+
+	client.SetRetryCount(3)
 
 	httpClient := &httpClient{
-		client: innerClient,
+		client: client,
 	}
 
 	return httpClient
