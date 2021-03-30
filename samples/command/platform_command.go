8ffe5260db869a6e6e751664e763f43ae6b45bdb
ctlove0523 <478309639@qq.com>
2021-03-30 21:51:28 +0800

    prepare for async

--- samples/command/platform_command.go
+++ samples/command/platform_command.go
@@ -4,27 +4,21 @@ import (
 	"fmt"
 	"github.com/ctlove0523/huaweicloud-iot-device-sdk-go"
 	"github.com/ctlove0523/huaweicloud-iot-device-sdk-go/samples"
-	mqtt "github.com/eclipse/paho.mqtt.golang"
-	"log"
-	"os"
 	"time"
 )
 
 // 处理平台下发的同步命令
 func main() {
-	mqtt.ERROR = log.New(os.Stdout, "[ERROR] ", 0)
-	mqtt.CRITICAL = log.New(os.Stdout, "[CRIT] ", 0)
-	mqtt.WARN = log.New(os.Stdout, "[WARN]  ", 0)
-	mqtt.DEBUG = log.New(os.Stdout, "[DEBUG] ", 0)
 	// 创建一个设备并初始化
 	device := samples.CreateDevice()
 
 	device.Init()
 
 	// 添加用于处理平台下发命令的callback
+	commandProcessResult:=false
 	device.AddCommandHandler(func(command iot.Command) bool {
 		fmt.Println("I get command from platform")
-		time.Sleep(10 * time.Second)
+		commandProcessResult = true
 		return true
 	})
 	time.Sleep(10 * time.Minute)
